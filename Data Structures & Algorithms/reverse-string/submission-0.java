c5756baee85c39d6aaf825b26695de4a21bd1092
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-07-05 18:29:11 -0600

    Update: reverse-string - submission-0

--- Data Structures & Algorithms/reverse-string/submission-0.java
+++ Data Structures & Algorithms/reverse-string/submission-0.java
@@ -1,16 +1,14 @@
 class Solution {
     public void reverseString(char[] s) {
-        char[] ans = new char[s.length];
-        int counter = 1;
-        for(int i = 0; i < s.length; i++){
-            for(int j = s.length - counter; j > i; j--){
-                char temp = s[i];
-                s[i] = s[j];
-                s[j] = temp;
-                break;
-            }
-            counter++;
+        int left = 0, right = s.length - 1;
+
+        while(left < right){
+            char placeHolder = s[left];
+            s[left] = s[right];
+            s[right] = placeHolder;
+
+            left++;
+            right--;
         }
-        
     }
 }
