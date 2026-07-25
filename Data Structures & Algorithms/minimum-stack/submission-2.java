99d4f0bb491b6e8df65126e0fd7040f480a9a042
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-07-25 11:33:56 -0600

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.java
@@ -0,0 +1,32 @@
+class MinStack {
+    private Stack<Integer> userStack;
+    private Stack<Integer> minStack;
+
+    public MinStack() {
+        userStack = new Stack();
+        minStack = new Stack();
+    }
+    
+    public void push(int val) {
+        userStack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(userStack.isEmpty()) return;
+        int top = userStack.pop();
+        if(minStack.peek() == top){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return userStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
