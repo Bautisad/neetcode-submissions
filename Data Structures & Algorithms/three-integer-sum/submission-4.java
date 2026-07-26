9015ef9a75809ae8b1827d9c030d128462222386
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-07-26 11:44:03 -0600

    Update: three-integer-sum - submission-4

--- Data Structures & Algorithms/three-integer-sum/submission-4.java
+++ Data Structures & Algorithms/three-integer-sum/submission-4.java
@@ -5,25 +5,33 @@ class Solution {
         Arrays.sort(nums);
 
         for(int i = 0; i < nums.length; i++){
-            int left = i + 1, right = nums.length - 1;
-            if(i != 0 && nums[i] == nums[i - 1]) continue;
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            if(i > 0 && nums[i] == nums[i - 1]) continue;
+
             while(left < right){
-                int currSum = nums[i] + nums[left] + nums[right];
-                if(currSum == 0){
+                int curSum = nums[i] + nums[left] + nums[right];
+
+                if(curSum == 0){
                     ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
-                    while(left < right && nums[left] == nums[left -1]){
+
+                    while(left < right && nums[left] == nums[left - 1]){
                         left++;
                     }
-
-                }else if(currSum > 0){
+                } else if(curSum > 0){
                     right--;
-                } else if (currSum < 0){
+                } else if(curSum < 0){
                     left++;
+                } else {
+                    left++;
+                    right--;
                 }
-            }
+            }            
         }
+
         return ans;
     }
 }
