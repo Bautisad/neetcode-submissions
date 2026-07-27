3a2a99933252b98363eaff39d9ad6017ac290958
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-07-27 15:18:42 -0600

    Update: validate-parentheses - submission-7

--- Data Structures & Algorithms/validate-parentheses/submission-7.java
+++ Data Structures & Algorithms/validate-parentheses/submission-7.java
@@ -1,26 +1,22 @@
 class Solution {
     public boolean isValid(String s) {
-        Map<Character,Character> keyList = new HashMap<>();
+        Map<Character, Character> ans = new HashMap<>();
 
-        keyList.put('}', '{');
-        keyList.put(']', '[');
-        keyList.put(')', '(');
-
-        char[] sCharArray = s.toCharArray();
-
-        if(sCharArray.length < 2) return false;
+        ans.put('}', '{');
+        ans.put(']', '[');
+        ans.put(')', '(');
 
         Stack<Character> curStack = new Stack<>();
 
-        for(int i = 0; i < sCharArray.length; i++){
-            if(keyList.containsKey(sCharArray[i])){
-                if(!curStack.isEmpty() && curStack.peek() == keyList.get(sCharArray[i])){
+        for(char x : s.toCharArray()){
+            if(ans.containsKey(x)){
+                if(!curStack.isEmpty() && curStack.peek() == ans.get(x)){
                     curStack.pop();
                 } else {
                     return false;
                 }
             } else {
-                curStack.push(sCharArray[i]);
+                curStack.push(x);
             }
         }
 
