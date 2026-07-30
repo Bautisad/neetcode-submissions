ecdc8d7e6a0e87e422658af71afa6c73e598389b
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-07-30 15:46:35 -0600

    Update: three-integer-sum - submission-7

--- Data Structures & Algorithms/three-integer-sum/submission-7.java
+++ Data Structures & Algorithms/three-integer-sum/submission-7.java
@@ -1,15 +1,22 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> ans = new ArrayList<>();
-        Arrays.sort(nums);
-        for(int i = 0; i < nums.length; i++){
-            int left= i + 1, right = nums.length - 1;
 
-            if(i != 0 && nums[i] == nums[i - 1]) continue;
+        Arrays.sort(nums);
+
+        List<List<Integer>> ans = new ArrayList<>();
+
+        for(int i = 0; i < nums.length; i++){
+            int left = i + 1;
+            int right = nums.length - 1;
+
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
 
             while(left < right){
                 int sum = nums[i] + nums[left] + nums[right];
-                if(sum == 0) {
+
+                if(sum == 0){
                     ans.add(Arrays.asList(nums[i], nums[left], nums[right]));
                     left++;
                     right--;
@@ -17,14 +24,17 @@ class Solution {
                     while(left < right && nums[left] == nums[left - 1]){
                         left++;
                     }
-
+                } else if(sum > 0){
+                    right--;
                 } else if (sum < 0){
                     left++;
-                } else if (sum > 0){
+                } else {
+                    left++;
                     right--;
                 }
             }
         }
         return ans;
+
     }
 }
