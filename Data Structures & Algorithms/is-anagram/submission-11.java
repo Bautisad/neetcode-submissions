df3d30dc6327acd0d5fe2ef881110c5a98c3a181
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-07-31 10:03:39 -0600

    Update: is-anagram - submission-11

--- Data Structures & Algorithms/is-anagram/submission-11.java
+++ Data Structures & Algorithms/is-anagram/submission-11.java
@@ -1,18 +1,17 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        char[] sCharArray = s.toCharArray();
-        char[] tCharArray = t.toCharArray();
-        
-        Arrays.sort(sCharArray);
-        Arrays.sort(tCharArray);
-
-        if(sCharArray.length != tCharArray.length) return false;
-
-        for(int i = 0; i < sCharArray.length; i++){
-            if(sCharArray[i] != tCharArray[i]){
-                return false;
-            }
+        if(s.length() != t.length()){
+            return false;
         }
-        return true;
+
+        Map<Character,Integer> sMap = new HashMap<>();
+        Map<Character, Integer> tMap = new HashMap<>();
+
+        for(int i = 0; i < s.length(); i++){
+            sMap.put(s.charAt(i), sMap.getOrDefault(s.charAt(i), 0) + 1);
+            tMap.put(t.charAt(i), tMap.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        return sMap.equals(tMap);
     }
 }
