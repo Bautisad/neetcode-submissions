eec88512b551ec8553b39487839ef8c6c5bdcb41
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-07-31 10:00:57 -0600

    Update: duplicate-integer - submission-8

--- Data Structures & Algorithms/duplicate-integer/submission-8.java
+++ Data Structures & Algorithms/duplicate-integer/submission-8.java
@@ -1,11 +1,12 @@
 class Solution {
     public boolean hasDuplicate(int[] nums) {
-        Set<Integer> keyList = new HashSet<>();
+        Set<Integer> ans = new HashSet<>();
+
         for(int i = 0; i < nums.length; i++){
-            if(keyList.contains(nums[i])){
+            if(ans.contains(nums[i])){
                 return true;
             } else {
-                keyList.add(nums[i]);
+                ans.add(nums[i]);
             }
         }
         return false;
