6682e2f86772c1cffdf716f66711481bba437ea3
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-08-01 19:53:47 -0600

    Add: string-encode-and-decode - submission-3

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-3.java
@@ -0,0 +1,35 @@
+class Solution {
+
+    public String encode(List<String> strs) {
+        StringBuilder ans = new StringBuilder();
+
+        for(String x : strs){
+            ans.append(x.length()).append('#').append(x);
+        }
+
+        return ans.toString();
+    }
+
+    public List<String> decode(String str) {
+        List<String> ans = new ArrayList<>();
+
+        int left = 0;
+        //3#cat
+        while(left < str.length()){
+            int right = left;
+
+            while(str.charAt(right) != '#'){
+                right++;
+            }
+
+            int length = Integer.parseInt(str.substring(left, right));
+            left = right + 1;
+            right = left + length;
+
+            String curWord = str.substring(left,right);
+            ans.add(curWord);
+            left = right;
+        }
+        return ans;
+    }
+}
