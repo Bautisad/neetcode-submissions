17c617a3a3b65cdc13e280124ac2418b375e2a2f
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-08-02 09:23:21 -0600

    Update: products-of-array-discluding-self - submission-0

--- Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
+++ Data Structures & Algorithms/products-of-array-discluding-self/submission-0.java
@@ -1,34 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int index = nums.length;
-        int[] prefix = new int[index];
-        int[] postfix = new int[index];
-        int[] ans = new int[index];
+        int[] ans = new int[nums.length];
+        int prefix = 1;
 
-        for(int i = 0; i < index; i++){
-            if(i == 0){
-                prefix[i] = nums[i] * 1;
-            } else {
-                prefix[i] = nums[i] * prefix[i - 1];
-            }
+        ans[0] = 1;
+        for(int i = 1; i < ans.length; i++){
+            ans[i] = ans[i - 1] * nums[i - 1];
         }
 
-        for(int i = index - 1; i > -1; i--){
-            if(i == index - 1){
-                postfix[i] = nums[i] * 1;
-            } else {
-                postfix[i] = nums[i] * postfix[i + 1];
-            }
-        }
-
-        for(int i = 0; i < index; i++){
-            if(i == 0){
-                ans[i] = postfix[i + 1] * 1;
-            } else if(i == index - 1){
-                ans[i] = prefix[i - 1] * 1;
-            } else {
-                ans[i] = postfix[i + 1] * prefix[i - 1];
-            }
+        int postfix = 1;
+        for(int i = ans.length - 1; i >= 0; i--){
+            ans[i] *= postfix;
+            postfix *= nums[i];
         }
         return ans;
     }
