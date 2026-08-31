833c822fdf4f78b7f0f88b66ed304790f5693e0e
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-08-31 16:45:16 -0600

    Add: minimum-stack - submission-9

---
+++ Data Structures & Algorithms/minimum-stack/submission-9.java
@@ -0,0 +1,32 @@
+class MinStack {
+    Stack<Integer> userStack;
+    Stack<Integer> minStack;
+
+    public MinStack() {
+        userStack = new Stack<>();
+        minStack = new Stack<>();
+    }
+    
+    public void push(int val) {
+        userStack.push(val);
+        if(minStack.isEmpty() || val <= minStack.peek()){
+            minStack.push(val);
+        }
+    }
+    
+    public void pop() {
+        if(userStack.isEmpty()) return;
+        int val = userStack.pop();
+        if(val == minStack.peek()){
+            minStack.pop();
+        }
+    }
+    
+    public int top() {
+        return userStack.peek();
+    }
+    
+    public int getMin() {
+        return minStack.peek();
+    }
+}
