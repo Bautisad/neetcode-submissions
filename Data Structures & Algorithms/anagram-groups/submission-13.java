f96dc356b177972a90178780d5a38b1f0277c1a0
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-08-31 17:19:02 -0600

    Add: anagram-groups - submission-13

---
+++ Data Structures & Algorithms/anagram-groups/submission-13.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> ans = new HashMap<>();
+
+        for(int i = 0; i < strs.length; i++){
+            int[] letter = new int[26];
+            for(char x : strs[i].toCharArray()){
+                letter[x - 'a']++;
+            }
+
+            String key = Arrays.toString(letter);
+            ans.putIfAbsent(key, new ArrayList<>());
+            ans.get(key).add(strs[i]);
+        }
+        return new ArrayList(ans.values());
+    }
+}
