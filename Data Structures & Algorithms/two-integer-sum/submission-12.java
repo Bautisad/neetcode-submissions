2c9f37160e848c20ae49bf41b1ff56e227328fe6
Duke Bautista <164932563+Bautisad@users.noreply.github.com>
2026-09-04 21:15:08 -0600

    Update: two-integer-sum - submission-12

--- Data Structures & Algorithms/two-integer-sum/submission-12.java
+++ Data Structures & Algorithms/two-integer-sum/submission-12.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> keyList = new HashMap<>();
 
-        for(int i = 0; i < nums.length; i++){
-            for(int j = i +1; j < nums.length; j++){
-                int sum = nums[i] + nums[j];
-                if(sum == target){
-                    return new int[]{i,j};
-                }
+        for(int i =0; i < nums.length; i++){
+            int sum = target - nums[i];
+
+            if(keyList.containsKey(sum)){
+                return new int[]{keyList.get(sum),i};
+            } else {
+                keyList.put(nums[i], i);
             }
         }
-
         return new int[]{};
     }
 }
